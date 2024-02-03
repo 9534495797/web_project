6af09a49c325f174e98ec7ffcf82eeaa05a89f71
9534495797 <chaubeyankit927@gmail.com>
2024-02-03 05:49:00 +0530

    ok

---
+++ resume.js
@@ -0,0 +1,11 @@
+// Add JavaScript code as needed for interactivity
+// For example, smooth scrolling to anchor links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
